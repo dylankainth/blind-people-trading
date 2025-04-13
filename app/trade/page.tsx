8332429c1b640f23d8dc2cb8c3e2b5fa3ddd933b
Dylan Kainth <dylan.kainth@kcl.ac.uk>
2025-04-13 08:57:26 +0000

    setup data pulling from fastapi backend for graph historical-data display

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,19 +1,35 @@
 'use client'
-import { simulateData } from "@/components/graph-view";
 import GraphView from "@/components/graph-view";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Star } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const data = simulateData();
+
+    // make a request to http://localhost:8000/price?symbol=SOL-USD&interval=1mo&range=max
+
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:8000/price?symbol=SOL-USD&interval=1mo&range=max");
+            const result = await response.json();
+            setData(result);
+        };
+
+        fetchData();
+    }
+        , []);
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-start sm:justify-start md:justify-center px-4 sm:px-8 md:px-12 lg:px-24 -my-25">
 
             <div className="flex justify-center w-full">
                 <div className="flex flex-col items-center max-w-[1500px] w-full">
                     <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] mb-12 lg:mb-0 lg:hidden">
-                        <GraphView data={data} />
+                        {/* <GraphView data={data} /> */}
+                        {JSON.stringify(data)}
                     </div>
                     <div className="flex flex-col lg:flex-row items-center w-full">
                         <div className="w-full lg:w-1/3 lg:pr-8 mb-6 lg:mb-0">
